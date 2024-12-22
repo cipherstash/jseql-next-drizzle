e46f802872a3b1aa01d31e4b6d1cafcdd399aca5
CJ Brewer <brewercalvinj@gmail.com>
2024-12-22 17:20:46 -0600

    fix: add webpack config for .node files

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,25 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "cipherstash.com",
-			},
-		],
-	},
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cipherstash.com',
+      },
+    ],
+  },
+  webpack: (config, { isServer }) => {
+    // Only apply this rule on the server build, because
+    // .node modules can’t run in a browser environment
+    if (isServer) {
+      config.module.rules.push({
+        test: /\.node$/,
+        use: 'node-loader',
+      })
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
