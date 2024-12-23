3d3b53d50c5be8e366b9a7d165cf22e2f43fbcdb
Drew Thomas <drew@cipherstash.com>
2024-12-23 13:36:44 +1100

    Add @cipherstash/jseql to external packages in next.config.ts
    Opt out of having Next.js bundle jseql and native Node.js require instead.
    
    See:
    https://nextjs.org/docs/app/api-reference/config/next-config-js/serverExternalPackages

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "cipherstash.com",
-			},
-		],
-	},
-};
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cipherstash.com',
+      },
+    ],
+  },
+  serverExternalPackages: ['@cipherstash/jseql'],
+}
 
 export default nextConfig;
